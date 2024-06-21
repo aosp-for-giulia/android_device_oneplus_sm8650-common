dc9097267a31bf05b42c8ac311409ca2c8f095d0
Sugakesshaa <sugakesshaa@gmail.com>
2024-06-21 11:42:50 +0530

    sm8650-common: Drop OplusDoze
    * in favour of our custom multihal service and aosp lift to wake service
    
    Change-Id: I569bc0a5d6f94868f28cb815b59edcc3196806eb
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -139,13 +139,6 @@ PRODUCT_COPY_FILES += \
 # Dolby
 $(call inherit-product, vendor/sony/dolby/sonydolby.mk)
 
-# Doze
-ifneq ($(TARGET_IS_TABLET),true)
-PRODUCT_PACKAGES += \
-    OplusDoze \
-    OplusDozeResCommon
-endif
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
@@ -364,7 +357,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors-service.oplus-multihal \
     sensors.dynamic_sensor_hal \
     sensors.oplus
 
