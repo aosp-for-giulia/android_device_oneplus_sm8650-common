1fa247b493f1dd61fe8c046cfb5dbd8ab6787cb7
spkal01 <kalligeross@gmail.com>
2026-06-07 19:57:24 +0300

    sm8650-common: extract-files: Patch public-libraries.txt to allow opcamera to access arcsoft libs

--- extract-files.py
+++ extract-files.py
@@ -124,6 +124,18 @@ blob_fixups: blob_fixups_user_type = {
         ),
     'vendor/etc/pwr/PowerFeatureConfig.xml': blob_fixup()
         .regex_replace(r'(<Name>GamePowerOptFeature</Name>\s*<Enable>)0(<\/Enable>)', r'\g<1>1\g<2>'),
+    'vendor/etc/public.libraries.txt': blob_fixup()
+        .add_line_if_missing('libarcsoft_hdr_couple_api.so')
+        .add_line_if_missing('libarcsoft_high_dynamic_range_couple.so')
+        .add_line_if_missing('libarcsoft_smart_denoise.so')
+        .add_line_if_missing('libarcsoft_turbo_hdr_raw.so')
+        .add_line_if_missing('libarcsoft_turbo_raw.so')
+        .add_line_if_missing('libarcsoft_qnnhtp.so')
+        .add_line_if_missing('libQnnHtp.so')
+        .add_line_if_missing('libQnnSystem.so')
+        .add_line_if_missing('libQnnHtpV79Stub.so')
+        .add_line_if_missing('libQnnGpu.so')
+        .add_line_if_missing('libQnnHtpStub.so'),
     'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
         .add_line_if_missing('sched_get_priority_min: 1')
         .add_line_if_missing('sched_get_priority_max: 1'),
