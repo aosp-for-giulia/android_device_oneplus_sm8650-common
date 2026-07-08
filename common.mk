d17df1878884dec2b7661f40ed917284c9945512
John Galt <johngaltfirstrun@gmail.com>
2026-07-08 17:04:25 -0400

    sm8650-common: build passthrough charging package
    We do not have real bypass charging support, however passthrough
    charging is still useful in some situations:
     - In sustained passively-cooled performance scenarios with thermal
       throttling disabled, battery heats less than with discharging or
       charging.
     - It is likely healthier than charging to 100% and leaving it plugged
       in in certain scenarios like above.

--- common.mk
+++ common.mk
@@ -100,6 +100,10 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+# Passthrough charging
+PRODUCT_PACKAGES += \
+    BypassCharge
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
