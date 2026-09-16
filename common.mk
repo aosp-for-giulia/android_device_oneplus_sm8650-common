89fec40188caba0486616efa5d93bd1c10e08835
Omar Aguilar <omarag9099@gmail.com>
2026-09-16 10:00:08 -0600

    sm8650-common: Build MatLog
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- common.mk
+++ common.mk
@@ -270,6 +270,10 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.oplus
 
+# MatLog
+PRODUCT_PACKAGES += \
+    MatLog
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
